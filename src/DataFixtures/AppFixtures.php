bd28897f2208b03d7a33f690c9b3d713ccd61ecb
Magali Rézeau <magalirezeau@gmail.com>
2020-08-18 14:53:25 +0200

    add data-fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,59 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Phone;
+use App\Entity\Company;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $brands = ['iPhone', 'Samsung', 'Xiaomi', 'Huawei', 'Sony'];
+    private $colors = ['black', 'white', 'red', 'pink', 'gray'];
+    private $storages = ['32go', '64go', '256go'];
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 20; $i++) {
+            $phone = new Phone();
+            $phone
+                ->setBrand($this->brands[rand(0, 4)] . ' ' . rand(4, 8))
+                ->setColor($this->colors[rand(0, 4)])
+                ->setReference(rand(1000, 5000))
+                ->setPrice(rand(500, 1000))
+                ->setStorage($this->storages[rand(0, 2)]);
+
+            $manager->persist($phone);
+        }
+
+        $company = new Company();
+        $company
+            ->setEmail('company@gmail.com')
+            ->setPassword($this->encoder->encodePassword($company, 'password'))
+            ->setRoles(['ROLE_COMPANY']);
+
+        $manager->persist($company);
+
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user
+                ->setFirstname($faker->firstname)
+                ->setLastname($faker->lastname)
+                ->setEmail($faker->email)
+                ->setCompany($company);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
